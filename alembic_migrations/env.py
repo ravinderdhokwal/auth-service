128b454273c163823920b3b622d9eca93af62ca2
ravinderdhokwal <ravindercodes@gmail.com>
2026-08-29 23:09:09 +0530

    update alembic configuration to use runtime DATABASE_URL, set target metadata and import data models

--- alembic_migrations/env.py
+++ alembic_migrations/env.py
@@ -7,9 +7,18 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+# project's config, metadata and models
+from auth_service.core.config import settings
+from auth_service.db.base import Base
+import auth_service.models
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# Inject app's real DATABASE_URL into Alembic's config at runtime,
+# overriding whatever (nothing, now) is in alembic.ini.
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,7 +29,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
