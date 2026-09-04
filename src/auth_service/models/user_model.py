ef87f52d903dd3c814a425bdc9f912858be89e26
ravinderdhokwal <ravindercodes@gmail.com>
2026-09-04 19:40:06 +0530

    update user model

--- src/auth_service/models/user_model.py
+++ src/auth_service/models/user_model.py
@@ -11,7 +11,7 @@ class UserModel(Base):
     id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
     full_name: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(50), nullable=False, index=True, unique=True)
-    password: Mapped[str] = mapped_column(String(50), nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
 
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
